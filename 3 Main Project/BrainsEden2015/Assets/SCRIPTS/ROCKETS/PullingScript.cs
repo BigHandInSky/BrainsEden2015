c8e686b50075038421df80cd5e5f1a347ab77d2d
Robert Billings <thellos64@gmail.com>
2015-06-27 16:48:50 +0100

    Pulling towards rocket
    Added rocket that pulls debris and junk towards itself. YAY

---
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/PullingScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PullingScript: MonoBehaviour {
+
+	public float maxDistance;
+	
+	public GameObject junkPrefab;
+	public GameObject[] junk;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if (junk == null) 
+		{
+			//Nothing
+		}
+		else 
+		{
+			junk = GameObject.FindGameObjectsWithTag ("Junk");
+			
+			foreach (GameObject junkPrefab in junk) 
+			{
+				float distance = Vector3.Distance (gameObject.transform.position, junkPrefab.transform.position);
+				
+				if (distance < maxDistance) 
+				{
+					Vector3 velocity = transform.position - junkPrefab.transform.position;
+					junkPrefab.GetComponent<Rigidbody> ().AddForce (velocity * 10f);
+				}
+			}
+		}
+	}
+}
