b10ec47cbdce8ec8f15afeef5b8d8fac161eeccf
BigHandInSky <bighandinsky@gmail.com>
2015-06-27 08:50:31 +0100

    First Merge - Prototype is close!

---
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/MovingTowards.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovingTowards : MonoBehaviour {
+
+	public float moveTowards = 1.0f;
+
+	public GameObject a;
+	public GameObject b;
+	public GameObject c;
+	public GameObject d;
+
+	List<GameObject> fragments;
+
+	// Use this for initialization
+	void Start () {
+		fragments = new List<GameObject> ();
+		fragments.Add (a);
+		fragments.Add (b);
+		fragments.Add (c);
+		fragments.Add (d);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+
+			for(int i=0; i < fragments.Capacity; i++){
+
+				Vector3 velocity = transform.position - fragments[i].transform.position;
+				fragments[i].GetComponent<Rigidbody>().AddForce(velocity* 1500);
+
+				//Vector3 move = new Vector3 (transform.position.x + moveTowards, transform.position.y, transform.position.z);
+				//transform.position = move;
+
+			}
+		}
+	}
+}
