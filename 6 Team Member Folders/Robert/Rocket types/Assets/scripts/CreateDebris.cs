d05349f105cad35033bea9ea41b742ea0170cef8
Robert Billings <thellos64@gmail.com>
2015-06-27 08:47:43 +0100

    Debris Creating Rocket
    Spawns debris randomly around it when you press Space.

---
+++ 6 Team Member Folders/Robert/Rocket types/Assets/scripts/CreateDebris.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateDebris : MonoBehaviour {
+
+	public GameObject debris;
+	public float minValue;
+	public float maxValue;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			Vector3 position = new Vector3(Random.Range(minValue, maxValue), 1, Random.Range(minValue, maxValue));
+			Instantiate(debris, position, Quaternion.identity);
+			//Destroy(gameObject);
+		}
+	}
+}
