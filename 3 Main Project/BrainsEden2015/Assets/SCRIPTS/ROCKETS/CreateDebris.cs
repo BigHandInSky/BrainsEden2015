c50d3436f51ea2ba686985dd93e917994ebe9baf
Robert Billings <thellos64@gmail.com>
2015-06-27 19:45:20 +0100

    Reapplied debris
    Debris reapplied.

--- 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/CreateDebris.cs
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/CreateDebris.cs
@@ -4,15 +4,33 @@ using System.Collections.Generic;
 
 public class CreateDebris : MonoBehaviour {
 
+	public GameObject debris1;
+	public GameObject debris2;
+	public GameObject debris3;
+	public GameObject debris4;
+	
+	List<GameObject> debrisList;
+
 	public GameObject debris;
 	public float minValue;
 	public float maxValue;
 
 
-	private float delayTimer = 1f;
+	private float delayTimer = 1.5f;
 	private float junkTimer = 0;
 
+	// Use this for initialization
+	void Start () 
+	{
+		debrisList = new List<GameObject> ();
+		debrisList.Add (debris1);
+		debrisList.Add (debris2);
+		debrisList.Add (debris3);
+		debrisList.Add (debris4);
 
+	}
+	
+	// Update is called once per frame
 	void Update () 
 	{
 		delayTimer -= Time.deltaTime;
@@ -20,10 +38,10 @@ public class CreateDebris : MonoBehaviour {
 		if(delayTimer <= 0 && GetComponent<Rigidbody> ().velocity.magnitude > 3){
 			//Vector3 position = new Vector3(Random.Range(minValue, maxValue), Random.Range(minValue, maxValue), 0);
 			if(junkTimer <= 0){
-				Instantiate(debris, transform.position - transform.up, Quaternion.identity);
+				Instantiate(debrisList[Random.Range(0,3)], transform.position - transform.up, Quaternion.identity);
 				junkTimer = 1;
 			}
-			junkTimer -= Time.deltaTime * 2.5f;
+			junkTimer -= Time.deltaTime;
 		}
 	}
 }
