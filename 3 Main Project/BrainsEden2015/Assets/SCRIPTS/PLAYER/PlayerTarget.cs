15999a801dceaa74838a6d381291c1030e9bd217
asa47 <asa47@hotmail.it>
2015-06-27 10:28:37 +0100

    test

--- 3 Main Project/BrainsEden2015/Assets/SCRIPTS/PLAYER/PlayerTarget.cs
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/PLAYER/PlayerTarget.cs
@@ -2,42 +2,50 @@
 using System.Collections;
 
 public class PlayerTarget : MonoBehaviour {
-
+	
 	public GameObject bulletPrefab;
-
+	public GameObject muzzle;
+	
 	public float moveSpeed = 5;
 	public float rocketSpeed = 10;
-
+	
 	void Update() {
-
+		
 		if (Input.GetButtonDown ("Fire1")) {
-
+			
 			Vector3 mousePos = Input.mousePosition;
 			mousePos.z = -Camera.main.transform.position.z;
 			mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
+			
 			GameObject bullet = (GameObject)Instantiate(bulletPrefab,transform.position, Quaternion.identity);
-
+			
 			Vector3 dir = (mousePos - transform.position).normalized;
 			bullet.transform.eulerAngles = new Vector3(0, 0,90 + Mathf.Atan2(-dir.y, -dir.x) * 180/3.14f);
 			bullet.GetComponent<Rigidbody>().velocity = dir * rocketSpeed;
 		}
-
+		
 		if (Input.GetKey (KeyCode.A)) {
 			transform.Rotate(0, 0 , 30 * Time.deltaTime * moveSpeed, Space.World);
 		}
 		else if (Input.GetKey (KeyCode.D)) {
 			transform.Rotate(0, 0 , -30 * Time.deltaTime * moveSpeed, Space.World);
 		}
-
-
+		
+		
 		if (Input.GetKey (KeyCode.Space)) {
-
-			GameObject bullet = (GameObject)Instantiate(bulletPrefab,transform.position + transform.up, Quaternion.identity);
-			Vector3 velocity = transform.TransformDirection(transform.up * rocketSpeed);
+			
+			GameObject bullet = (GameObject)Instantiate(bulletPrefab,muzzle.transform.position, Quaternion.identity);
+			
+			//float angle = Mathf.Atan2(muzzle.transform.localPosition.y, muzzle.transform.localPosition.x);
+			//Debug.Log (angle * 180 / 3.14f);
+			//bullet.transform.eulerAngles = new Vector3 (0, 0, angle * 180 / 3.14f);
+			//bullet.GetComponent<Rigidbody>().velocity = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+			
+			
+			Vector3 velocity = muzzle.transform.up * rocketSpeed;
 			bullet.GetComponent<Rigidbody>().velocity = velocity;
 			bullet.transform.eulerAngles = new Vector3 (0, 0, 90 + Mathf.Atan2 (-velocity.y, -velocity.x) * 180 / 3.14f);
 		}
 	}
-
+	
 }
--- 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/RocketOrbitBehavior.cs
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/RocketOrbitBehavior.cs
@@ -2,23 +2,36 @@
 using System.Collections;
 
 public class RocketOrbitBehavior : MonoBehaviour {
-
-
-	float GravIntensity = 1;
+	
+	
+	float GravIntensity = 2.5f;
 	float maxSpeed = 10;
-
+	float timer = 2;
+	
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 velocity = GetComponent<Rigidbody> ().velocity;
-		float dist = Vector3.Distance(Vector3.zero, gameObject.transform.position );
-		velocity += ( (Vector3.zero - gameObject.transform.position)/dist/dist*GravIntensity);
-		GetComponent<Rigidbody> ().velocity = velocity.normalized * maxSpeed; 
-		transform.eulerAngles = new Vector3 (0, 0, 90 + Mathf.Atan2 (-velocity.y, -velocity.x) * 180 / 3.14f);
+		Debug.Log (maxSpeed);
 
-		maxSpeed *= 0.999f;
-		if (maxSpeed <= 0)
-			maxSpeed = 0;
+		if (maxSpeed >= 0) 
+		{
+			GravIntensity *= 0.999f;
+			Vector3 velocity = GetComponent<Rigidbody> ().velocity;
+			float dist = Vector3.Distance (Vector3.zero, gameObject.transform.position);
+			velocity += ((Vector3.zero - gameObject.transform.position) / dist / dist * GravIntensity);
+			GetComponent<Rigidbody> ().velocity = velocity.normalized * maxSpeed; 
+			transform.eulerAngles = new Vector3 (0, 0, 90 + Mathf.Atan2 (-velocity.y, -velocity.x) * 180 / 3.14f);
+			
+			maxSpeed -= 10 * 0.001f;
+		}
+		else{
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		}
+		
+	}
 
+	void OnTriggerEnter(Collider other) {
+		if(other.name == "World")
+			Destroy(gameObject);
 	}
 }
