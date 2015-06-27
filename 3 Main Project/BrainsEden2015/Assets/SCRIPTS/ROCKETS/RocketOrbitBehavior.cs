c121fcd8a10ba997a070a02a45b49cc837a4e9a2
asa47 <asa47@hotmail.it>
2015-06-27 17:55:20 +0100

    orbit behavior improved

--- 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/RocketOrbitBehavior.cs
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ROCKETS/RocketOrbitBehavior.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class RocketOrbitBehavior : MonoBehaviour {
 	
 	
-	float GravIntensity = 2.5f;
-	float maxSpeed = 10;
+	float GravIntensity = 1.8f;
+	float maxSpeed = 11;
 	float timer = 0;
 
 	bool stage1 = false;
@@ -17,43 +17,16 @@ public class RocketOrbitBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		if (maxSpeed >= 0) 
-		{
-			timer += Time.deltaTime;
-			if(GravIntensity > 1.3)
-				GravIntensity *= 0.99f;
+		timer += Time.deltaTime;
+		if(GravIntensity > 1)
+			GravIntensity *= 0.995f;
 
-			Vector3 velocity = GetComponent<Rigidbody> ().velocity;
-			float dist = Vector3.Distance (Vector3.zero, gameObject.transform.position);
-			velocity += ((Vector3.zero - gameObject.transform.position) / dist / dist * GravIntensity);
-			GetComponent<Rigidbody> ().velocity = velocity.normalized * maxSpeed; 
-			
-			//maxSpeed -= 10 * 0.001f;
-			//maxSpeed *= 0.993f;
+		Vector3 velocity = GetComponent<Rigidbody> ().velocity;
+		float dist = Vector3.Distance (Vector3.zero, gameObject.transform.position);
+		velocity += ((Vector3.zero - gameObject.transform.position) / dist / dist * GravIntensity);
+		GetComponent<Rigidbody> ().velocity = velocity.normalized * maxSpeed; 
+		transform.eulerAngles = new Vector3 (0, 0, 90 + Mathf.Atan2 (-velocity.y, -velocity.x) * 180 / 3.14f);
 
-			if(timer > 1.5f && !stage1)
-			{
-				//maxSpeed -= 2;
-				//stage1 = true;
-				//GravIntensity = 1;
-			}
-			if(timer > 2.5f && !stage2)
-			{
-				//maxSpeed -= 2;
-				//stage2 = true;
-			}
-			if(maxSpeed < 2f)
-			{
-				maxSpeed -= 0.1f;
-				if (maxSpeed <= 0)
-					maxSpeed = 0;
-			}else
-				transform.eulerAngles = new Vector3 (0, 0, 90 + Mathf.Atan2 (-velocity.y, -velocity.x) * 180 / 3.14f);
-
-		}
-		else{
-			GetComponent<Rigidbody> ().velocity = Vector3.zero;
-		}
 		
 	}
 
