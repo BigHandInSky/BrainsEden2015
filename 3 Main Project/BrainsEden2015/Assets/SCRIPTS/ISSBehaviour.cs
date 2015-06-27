37172ec16261bc1ab06e025fd660a9f47c5892c0
BigHandInSky <bighandinsky@gmail.com>
2015-06-27 17:24:46 +0100

    Dual ISSs, ISS Rotation, Pull Rocket, Rocket select fix

--- 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ISSBehaviour.cs
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ISSBehaviour.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class ISSBehaviour : MonoBehaviour 
 {
+    [SerializeField] private bool m_RedISS;
+
     [SerializeField] private string m_REDTagToDetect = "RocketRED";
     [SerializeField] private string m_BLUTagToDetect = "RocketBLU";
 
     void OnCollisionEnter(Collision _other)
     {
-        if (_other.gameObject.tag == m_REDTagToDetect)
+        if (_other.gameObject.tag == m_REDTagToDetect && !m_RedISS)
         {
             GameStateHandler.Instance.ISSHit(true);
             DestroyObject(_other.gameObject);
         }
-        else if (_other.gameObject.tag == m_BLUTagToDetect)
+        else if (_other.gameObject.tag == m_BLUTagToDetect && m_RedISS)
         {
             GameStateHandler.Instance.ISSHit(false);
             DestroyObject(_other.gameObject);
---
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/ISSRotatePointBehaviou.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISSRotatePointBehaviou : MonoBehaviour 
+{
+    [SerializeField] private bool m_Red;
+    private Transform m_TransformComponent;
+    private float m_Time = 0f;
+    private float m_MoveMod;
+    private float m_MoveSpeed = 10f;
+
+    void Start()
+    {
+        m_TransformComponent = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        m_Time += Time.deltaTime;
+        m_MoveMod = Mathf.Sin(m_Time * 0.15f);
+        float _speed = (m_MoveSpeed * Time.deltaTime) * m_MoveMod;
+        m_TransformComponent.Rotate(Vector3.forward, Mathf.Clamp(_speed, -0.5f, 0.5f));
+    }
+}
