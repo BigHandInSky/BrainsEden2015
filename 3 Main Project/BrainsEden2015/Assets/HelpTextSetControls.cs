457da62cb071b764f46f9dda2d6504879175f00e
BigHandInSky <bighandinsky@gmail.com>
2015-06-28 16:26:58 +0100

    how to play text

---
+++ 3 Main Project/BrainsEden2015/Assets/HelpTextSetControls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpTextSetControls : MonoBehaviour {
+
+    [SerializeField] private GameObject AndroidText;
+    [SerializeField] private GameObject DesktopText;
+
+	void Start () 
+    {
+        if (Application.isMobilePlatform)
+            AndroidText.SetActive(true);
+        else
+            DesktopText.SetActive(true);
+	}
+
+}
