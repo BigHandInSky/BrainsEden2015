c4d62e723f964913e649e93767a0d21a062b7467
Robert Billings <thellos64@gmail.com>
2015-06-28 09:04:29 +0100

    Earth pulls debris back
    The earth will pull debris back if it gets too far away from it

---
+++ 3 Main Project/BrainsEden2015/Assets/SCRIPTS/GRAVITY/PullObjectsBack.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PullObjectsBack : MonoBehaviour {
+
+	public float maxDistance;
+	public float minDistance;
+
+	public float pullForce;
+	
+	//public GameObject target;
+	public GameObject[] targeted;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (targeted == null) 
+		{
+			//Nothing
+		}
+		else 
+		{
+			targeted = GameObject.FindGameObjectsWithTag ("Junk");
+			
+			foreach (GameObject target in targeted) 
+			{
+				float distance = Vector3.Distance (gameObject.transform.position, target.transform.position);
+				
+				if (distance < maxDistance && distance > minDistance) 
+				{
+					Vector3 velocity = transform.position - target.transform.position;
+					target.GetComponent<Rigidbody> ().AddForce (velocity * pullForce);
+				}
+			}
+		}
+	
+	}
+}
